7fb08cdeb8d99fb5cc78bed0f6a39f94c7abf296
anthod57 <87667439+anthod57@users.noreply.github.com>
2022-05-17 13:36:01 +0200

    Added price variation

--- src/components/CoinData.vue
+++ src/components/CoinData.vue
@@ -2,21 +2,27 @@
     <div class="container">
         <div class="wrapper">
             <div class="header">
-                <!-- Try to display cryptocurrency icon. Hide if the image cannot be loaded. -->
-                <img v-if="name" :src="`https://raw.githubusercontent.com/condacore/cryptocurrency-icons/master/64x64/${name.toLowerCase()}.png`" @error="(e) => handleImageLoadingError(e)"/>
-                <h2>{{name}}</h2>
+                <div class="left">
+                    <!-- Try to display cryptocurrency icon. Hide if the image cannot be loaded. -->
+                    <img v-if="name" :src="`https://raw.githubusercontent.com/condacore/cryptocurrency-icons/master/64x64/${name.toLowerCase()}.png`" @error="(e) => handleImageLoadingError(e)"/>
+                    <h2>{{name}}</h2>
+                </div>
+                <div class="right">
+                    <span class="price">{{price}}</span>
+                    <span v-bind:class="{'variation positive': variation >= 0, 'variation negative': variation < 0}">{{variation.toFixed(2)}}</span>
+                </div>
             </div>
-            <LineChart :chartData="chartData" />
-            <span>{{price}}</span>
+            <LineChart :chart-data="chartData" />
         </div>
     </div>
 </template>
 
 <script lang="ts">
 import { Endpoints, Timeframe } from '../endpoints';
+import { defineComponent } from "vue";
 import LineChart from './LineChart';
 import moment from 'moment';
-export default {
+export default defineComponent({
     name: 'CoinData',
     props: [
         'symbol',
@@ -41,7 +47,11 @@ export default {
             price : 0,
             priceHistory: Array<Number>(),
             timer: 0,
-            chartData: {}
+            variation: 0,
+            chartData: {
+                labels: Array<String>(),
+                datasets: Array<Object>()
+            }
         }
     },
 
@@ -64,6 +74,7 @@ export default {
         async updateData() {
             this.priceHistory = await this.getPriceHistory(this.symbol, Timeframe.Daily, 50);
             this.price = +Number.parseFloat(this.priceHistory[this.priceHistory.length - 1][4]).toFixed(2);
+            this.variation = this.dayVariation(this.price, this.priceHistory[this.priceHistory.length - 2][4]);
             this.updateChart();
         },
 
@@ -75,26 +86,30 @@ export default {
                 labels: labels,
                 datasets: [
                     {
-                    label: 'Price',
-                    backgroundColor: '#f87979',
-                    data: data
+                        label: 'Price',
+                        backgroundColor: '#f87979',
+                        data: data
                     }
                 ]
             }
+        },
+
+        dayVariation(currentPrice : number, yesterdayPrice : number) : number {
+            return (currentPrice - yesterdayPrice) / yesterdayPrice * 100;
         }
     },
 
     beforeUnmount() {
         clearInterval(this.timer);
     },
-}
+});
 </script>
 
 <style lang="scss" scoped>
     .container {
         width: 100%;
         height: auto;
-        max-width: 500px;
+        max-width: 800px;
         min-height: 300px;
         background-color: #2d254a;
         border-radius: 5px;
@@ -105,22 +120,82 @@ export default {
             display: flex;
             width: 100%;
             align-items: center;
-            justify-content: flex-start;
+            justify-content: space-between;
 
-            img {
-                max-width: 64px;
-                max-height: 64px;
-                width: 100%;
-                height: 100%;
-                object-fit: cover;
-                margin: 1rem;
+            .left {
+                display: flex;
+                align-items: center;
+                justify-content: flex-start;
+
+                
+                img {
+                    max-width: 50px;
+                    max-height: 50px;
+                    width: 100%;
+                    height: 100%;
+                    object-fit: cover;
+                    margin: 1rem;
+                }
+            }
+
+            .right {
+                display: flex;
+                align-items: center;
+                justify-content: flex-start;
+
+                .price {
+                    font-weight: bold;
+                    font-size: 1.5rem;  
+                    transition: opacity 0.5s ease;
+                    margin: 0 4px;
+
+                    &::before {
+                        content:'$'
+                    }
+                }
+
+                .variation {
+                    font-size: 1rem;
+                    margin: 0 4px;
+
+                    &::after {
+                        content:'%'
+                    }
+
+                    &.positive {
+                        color: #35b42e;
+                    }
+
+                    &.negative {
+                        color: #b4352e;
+                    }
+                }
             }
         }
 
-        
-        .linechart {
-            width: 60vw;
-            height: 50vh;
+        @media only screen and (max-width: 400px) {
+            padding: 0.5rem;
+            .header {
+                flex-direction: column;
+
+                h2 {
+                    font-size: 1rem;
+                }
+                
+                .left {
+                    img {
+                        max-width: 32px;
+                        max-height: 32px;
+                        margin: 0.5rem;
+                    }
+                }
+
+                .right {
+                    .price {
+                        font-size: 1rem;
+                    }
+                }
+            }
         }
     }
 </style>
